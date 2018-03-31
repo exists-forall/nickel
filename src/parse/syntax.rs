2707c073dd3f9a2b337147cf8f8011fc98e4ea25
William Brandon <hypercube97@gmail.com>
2018-03-31 02:33:59 +0000

    Add types to parser syntax

--- src/parse/syntax.rs
+++ src/parse/syntax.rs
@@ -1,5 +1,30 @@
+use types::Kind;
+
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct Ident {
     pub name: String,
     pub collision_id: usize,
+}
+
+#[derive(Clone, Debug)]
+pub struct TypeParam {
+    pub ident: Ident,
+    pub kind: Kind,
+}
+
+#[derive(Clone, Debug)]
+pub enum Type {
+    Unit,
+    Var { ident: Ident },
+    Exists { param: TypeParam, body: Box<Type> },
+    Func {
+        params: Vec<TypeParam>,
+        arg: Box<Type>,
+        ret: Box<Type>,
+    },
+    Pair { left: Box<Type>, right: Box<Type> },
+    App {
+        constructor: Box<Type>,
+        param: Box<Type>,
+    },
 }
